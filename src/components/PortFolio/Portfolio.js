8a56a6e2c83eb5f15704b8c3d0c49754b56d084d
Nipun <nipunpatel7@gmail.com>
2023-07-01 20:49:45 +0530

    updated resume and projects

--- src/components/PortFolio/Portfolio.js
+++ src/components/PortFolio/Portfolio.js
@@ -1,49 +1,79 @@
-import Card from '../Card/Card';
+import Card from "../Card/Card";
 
 const Portfolio = () => {
   const portFolio = [
     {
-      projectName: 'Todo',
-      link: 'https://github.com/Nipun65/Todo-Web-Application',
-      language: 'Angular',
-      description: 'A todo app :)',
+      projectName: "Todo",
+      link: "https://github.com/Nipun65/Todo-Web-Application",
+      language: "Angular",
+      description: "A todo app :)",
     },
     {
-      projectName: 'RandomMeme',
-      link: 'https://github.com/Nipun65/RandomMeme',
-      language: 'JavaScript',
-      description: 'Created browser extension which generate random meme',
+      projectName: "RandomMeme",
+      link: "https://github.com/Nipun65/RandomMeme",
+      language: "JavaScript",
+      description: "Created browser extension which generate random meme",
     },
     {
-      projectName: 'Video GamesDB',
-      link: 'https://github.com/Nipun65/VideogameDB',
-      language: 'ReactJS',
-      description: 'Shows the information about the games',
+      projectName: "Video GamesDB",
+      link: "https://github.com/Nipun65/VideogameDB",
+      language: "ReactJS",
+      description: "Shows the information about the games",
     },
     {
-      projectName: 'Online Car Parking Reservation',
-      link: 'https://github.com/Nipun65/Group5-Online_Car_Parking_Reservation_System',
-      language: 'JavaScript',
-      description: 'Website for booking online parking slot',
+      projectName: "Online Car Parking Reservation",
+      link: "https://github.com/Nipun65/Group5-Online_Car_Parking_Reservation_System",
+      language: "JavaScript",
+      description: "Website for booking online parking slot",
     },
     {
-      projectName: 'Login Page',
-      link: 'https://github.com/Nipun65/LogIn-Page',
-      language: 'ReactJS',
-      description: 'Login Page Demo',
+      projectName: "Login Page",
+      link: "https://github.com/Nipun65/LogIn-Page",
+      language: "ReactJS",
+      description: "Login Page Demo",
     },
-
     {
-      projectName: 'Expense List',
-      link: 'https://github.com/Nipun65/ExpenseList',
-      language: 'ReactJS',
-      description: 'Webapp for tracking expenses',
+      projectName: "Expense List",
+      link: "https://github.com/Nipun65/ExpenseList",
+      language: "ReactJS",
+      description: "Webapp for tracking expenses",
+    },
+    {
+      projectName: "Ecommerce Website",
+      link: "https://github.com/Nipun65/Ecommerce-Website",
+      language: "NodeJS",
+      description: "Ecommerce website to sell and buy products",
+    },
+    {
+      projectName: "SEO NextJS",
+      link: "https://github.com/Nipun65/nextjs-seo",
+      language: "NextJS",
+      description:
+        "Character Details of 'The Office': Identifying the Beloved Characters of the Show(SEO, Static HTML rendering using nextjs)",
+    },
+    {
+      projectName: "Todo App(Vuejs)",
+      link: "https://github.com/Nipun65/Todo-VueJS",
+      language: "VueJS",
+      description: "Todo app using vuejs",
+    },
+    {
+      projectName: "Snake",
+      link: "https://github.com/Nipun65/snake.js",
+      language: "Javascript",
+      description: "Snake game using Javascript",
+    },
+    {
+      projectName: "Tic-tac-toe",
+      link: "https://github.com/Nipun65/tictactoe",
+      language: "Javascript",
+      description: "Tic-tac-toe game using Javascript",
     },
   ];
   return (
-    <div id='portfolio' className='sectiontoppadding'>
+    <div id="portfolio" className="sectiontoppadding">
       <header>
-        <h2 className='h2 article-title'>Portfolio</h2>
+        <h2 className="h2 article-title">Portfolio</h2>
       </header>
       <Card portFolio={portFolio} />
     </div>
--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,54 +1,53 @@
-import styles from './Resume.module.css';
-import angularImg from '../../assets/imgData/angular.png';
-import typescriptImg from '../../assets/imgData/typescript.jpeg';
-import javascriptImg from '../../assets/imgData/javascript.png';
-import htmlImg from '../../assets/imgData/html.png';
-import pugImg from '../../assets/imgData/pug.svg';
-import cssImg from '../../assets/imgData/css.png';
-import pythonImg from '../../assets/imgData/python.png';
-import cplusImg from '../../assets/imgData/c++.png';
-import cImg from '../../assets/imgData/c.png';
-import firebaseImg from '../../assets/imgData/firebase.png';
-import jiraImg from '../../assets/imgData/jira.svg';
-import mongodb from '../../assets/imgData/mongodb.webp';
-import newTab from '../../assets/imgData/newTab.jpg';
-import experience from '../../assets/imgData/experience.png';
-import education from '../../assets/imgData/education.png';
-import react from '../../assets/imgData/react.png';
-import github from '../../assets/imgData/github.png';
-import gitlab from '../../assets/imgData/gitlab.png';
-import nextjs from '../../assets/imgData/nextjs.png';
-import nodejs from '../../assets/imgData/nodejs.png';
+import styles from "./Resume.module.css";
+import angularImg from "../../assets/imgData/angular.png";
+import typescriptImg from "../../assets/imgData/typescript.jpeg";
+import javascriptImg from "../../assets/imgData/javascript.png";
+import htmlImg from "../../assets/imgData/html.png";
+import pugImg from "../../assets/imgData/pug.svg";
+import cssImg from "../../assets/imgData/css.png";
+import pythonImg from "../../assets/imgData/python.png";
+import cplusImg from "../../assets/imgData/c++.png";
+import cImg from "../../assets/imgData/c.png";
+import firebaseImg from "../../assets/imgData/firebase.png";
+import jiraImg from "../../assets/imgData/jira.svg";
+import mongodb from "../../assets/imgData/mongodb.webp";
+import newTab from "../../assets/imgData/newTab.jpg";
+import experience from "../../assets/imgData/experience.png";
+import education from "../../assets/imgData/education.png";
+import react from "../../assets/imgData/react.png";
+import github from "../../assets/imgData/github.png";
+import gitlab from "../../assets/imgData/gitlab.png";
+import nextjs from "../../assets/imgData/nextjs.png";
+import nodejs from "../../assets/imgData/nodejs.png";
 
 const Resume = () => {
   return (
-    <div className='resume sectiontoppadding' data-page='resume' id='resume'>
+    <div className="resume sectiontoppadding" data-page="resume" id="resume">
       <header>
-        <h2 className='h2 article-title'>Resume</h2>
+        <h2 className="h2 article-title">Resume</h2>
         <a
-          href='https://drive.google.com/file/d/1Lh-KKiJ5NOpCVsZRGoaB6fqNrX29ykjs/view?usp=drive_link'
-          target='__blank'
-        >
+          href="https://drive.google.com/file/d/1Lh-KKiJ5NOpCVsZRGoaB6fqNrX29ykjs/view?usp=drive_link"
+          target="__blank">
           <img src={newTab} />
         </a>
       </header>
 
-      <section className='timeline'>
-        <div className='title-wrapper'>
-          <div className='icon-box'>
+      <section className="timeline">
+        <div className="title-wrapper">
+          <div className="icon-box">
             <img src={experience} className={styles.imageClass} />
           </div>
 
-          <h3 className='h3'>Experience</h3>
+          <h3 className="h3">Experience</h3>
         </div>
 
-        <ol className='timeline-list'>
-          <li className='timeline-item'>
-            <h4 className='h4 timeline-item-title'>Software Engineer</h4>
+        <ol className="timeline-list">
+          <li className="timeline-item">
+            <h4 className="h4 timeline-item-title">Software Engineer</h4>
             <span>Searce Inc</span>
             <span>Jul 2022 - Present</span>
 
-            <p className='timeline-text'>
+            <p className="timeline-text">
               <ul>
                 <li>
                   As a highly valued member of an Agile team. Leveraging my
@@ -90,12 +89,12 @@ const Resume = () => {
             </p>
           </li>
 
-          <li className='timeline-item' id='experience'>
-            <h4 className='h4 timeline-item-title'>Software Engineer Intern</h4>
+          <li className="timeline-item" id="experience">
+            <h4 className="h4 timeline-item-title">Software Engineer Intern</h4>
             <span>Searce Inc</span>
             <span>Jan 2022 - Jul 2022</span>
 
-            <p className='timeline-text'>
+            <p className="timeline-text">
               I used to manage the onboarding and offboarding process using HR
               software. Additionally, I demonstrated my expertise by developing
               multiple browser support extensions, including a LinkedIn
@@ -107,13 +106,13 @@ const Resume = () => {
             </p>
           </li>
 
-          <li className='timeline-item'>
-            <h4 className='h4 timeline-item-title'>Web Developer</h4>
+          <li className="timeline-item">
+            <h4 className="h4 timeline-item-title">Web Developer</h4>
 
             <span>EduCARE India NGO (Rural Internship)</span>
             <span>5th December 2019 - 31st December 2019</span>
 
-            <p className='timeline-text'>
+            <p className="timeline-text">
               I had the privilege of designing and developing a website for
               Educare India, an impactful platform that effectively connects
               passionate volunteers with NGOs, fostering meaningful
@@ -123,189 +122,172 @@ const Resume = () => {
         </ol>
       </section>
 
-      <section className='timeline'>
-        <div className='title-wrapper'>
-          <div className='icon-box'>
+      <section className="timeline">
+        <div className="title-wrapper">
+          <div className="icon-box">
             <img src={education} className={styles.imageClass} />
           </div>
 
-          <h3 className='h3'>Education</h3>
+          <h3 className="h3">Education</h3>
         </div>
 
-        <ol className='timeline-list'>
-          <li className='timeline-item'>
-            <h4 className='h4 timeline-item-title'>
+        <ol className="timeline-list">
+          <li className="timeline-item">
+            <h4 className="h4 timeline-item-title">
               Dhirubhai Ambani Institute of Information and Communication
               Technology
             </h4>
 
             <span>2018 — 2022</span>
 
-            <p className='timeline-text'>
+            <p className="timeline-text">
               Bachelor of Technology | Information and Communication Technology
             </p>
           </li>
 
-          <li className='timeline-item'>
-            <h4 className='h4 timeline-item-title'>
+          <li className="timeline-item">
+            <h4 className="h4 timeline-item-title">
               Shree Sarashwati Vidhyalaya(GSEB)
             </h4>
 
             <span>2016 — 2018</span>
 
-            <p className='timeline-text'>
-              Higher Secondary | Class XII Engineering
+            <p className="timeline-text">
+              Higher Secondary | Class XII Science
             </p>
           </li>
 
-          <li className='timeline-item'>
-            <h4 className='h4 timeline-item-title'>Komal Vidhyalay(GSEB)</h4>
+          <li className="timeline-item">
+            <h4 className="h4 timeline-item-title">Komal Vidhyalay(GSEB)</h4>
 
             <span>2015 — 2016</span>
 
-            <p className='timeline-text'>Secondary | Class X</p>
+            <p className="timeline-text">Secondary | Class X</p>
           </li>
         </ol>
       </section>
 
-      <section className='skill'>
-        <h3 className='h3 skills-title'>My skills</h3>
+      <section className="skill">
+        <h3 className="h3 skills-title">My skills</h3>
         <div className={styles.grid}>
           <a
             className={styles.imgcolumn}
-            href='https://reactjs.org/'
-            target='__blank'
-          >
-            <img src={react} alt='react-img-tag' />
+            href="https://reactjs.org/"
+            target="__blank">
+            <img src={react} alt="react-img-tag" />
             <p className={styles.skilltxt}>React</p>
           </a>
           <a
             className={styles.imgcolumn}
-            href='https://nextjs.org/'
-            target='__blank'
-          >
-            <img src={nextjs} alt='next-img-tag' />
+            href="https://nextjs.org/"
+            target="__blank">
+            <img src={nextjs} alt="next-img-tag" />
             <p className={styles.skilltxt}>Next.js</p>
           </a>
           <a
             className={styles.imgcolumn}
-            href='https://angular.io/'
-            target='__blank'
-          >
-            <img src={angularImg} alt='angular-img-tag' />
+            href="https://angular.io/"
+            target="__blank">
+            <img src={angularImg} alt="angular-img-tag" />
             <p className={styles.skilltxt}>Angular</p>
           </a>
           <a
             className={styles.imgcolumn}
-            href='https://javascript.info/'
-            target='__blank'
-          >
-            <img src={javascriptImg} alt='javascript-img-tag' />
+            href="https://javascript.info/"
+            target="__blank">
+            <img src={javascriptImg} alt="javascript-img-tag" />
             <p className={styles.skilltxt}>Javascript</p>
           </a>
           <a
             className={styles.imgcolumn}
-            href='https://www.typescriptlang.org/'
-            target='__blank'
-          >
-            <img src={typescriptImg} alt='typescript-img-tag' />
+            href="https://www.typescriptlang.org/"
+            target="__blank">
+            <img src={typescriptImg} alt="typescript-img-tag" />
             <p className={styles.skilltxt}>Typescript</p>
           </a>
 
           <a
             className={styles.imgcolumn}
-            href='https://developer.mozilla.org/en-US/docs/Glossary/HTML5'
-            target='__blank'
-          >
-            <img src={htmlImg} alt='html-img-tag' />
+            href="https://developer.mozilla.org/en-US/docs/Glossary/HTML5"
+            target="__blank">
+            <img src={htmlImg} alt="html-img-tag" />
             <p className={styles.skilltxt}>HTML</p>
           </a>
           <a
             className={styles.imgcolumn}
-            href='https://www.w3schools.com/css/'
-            target='__blank'
-          >
-            <img src={cssImg} alt='css-img-tag' />
+            href="https://www.w3schools.com/css/"
+            target="__blank">
+            <img src={cssImg} alt="css-img-tag" />
             <p className={styles.skilltxt}>CSS</p>
           </a>
           <a
             className={styles.imgcolumn}
-            href='https://pugjs.org/api/getting-started.html'
-            target='__blank'
-          >
-            <img src={pugImg} alt='pug-img-tag' />
+            href="https://pugjs.org/api/getting-started.html"
+            target="__blank">
+            <img src={pugImg} alt="pug-img-tag" />
             <p className={styles.skilltxt}>Pug</p>
           </a>
           <a
             className={styles.imgcolumn}
-            href='https://nodejs.org/en/docs'
-            target='__blank'
-          >
-            <img src={nodejs} alt='nodejs-img-tag' />
+            href="https://nodejs.org/en/docs"
+            target="__blank">
+            <img src={nodejs} alt="nodejs-img-tag" />
             <p className={styles.skilltxt}>Node.js</p>
           </a>
           <a
             className={styles.imgcolumn}
-            href='https://firebase.google.com/'
-            target='__blank'
-          >
-            <img src={firebaseImg} alt='firebase-img-tag' />
+            href="https://firebase.google.com/"
+            target="__blank">
+            <img src={firebaseImg} alt="firebase-img-tag" />
             <p className={styles.skilltxt}>Firebase</p>
           </a>
           <a
             className={styles.imgcolumn}
-            href='https://docs.github.com/en'
-            target='__blank'
-          >
-            <img src={github} alt='github-img-tag' />
+            href="https://docs.github.com/en"
+            target="__blank">
+            <img src={github} alt="github-img-tag" />
             <p className={styles.skilltxt}>Github</p>
           </a>
           <a
             className={`${styles.imgcolumn}`}
-            href='https://about.gitlab.com/'
-            target='__blank'
-          >
-            <img src={gitlab} alt='github-img-tag' />
+            href="https://about.gitlab.com/"
+            target="__blank">
+            <img src={gitlab} alt="github-img-tag" />
             <p className={styles.skilltxt}>Gitlab</p>
           </a>
           <a
             className={styles.imgcolumn}
-            href='https://www.atlassian.com/software/jira'
-            target='__blank'
-          >
-            <img src={jiraImg} alt='jira-img-tag' />
+            href="https://www.atlassian.com/software/jira"
+            target="__blank">
+            <img src={jiraImg} alt="jira-img-tag" />
             <p className={styles.skilltxt}>Jira</p>
           </a>
           <a
             className={styles.imgcolumn}
-            href='https://www.youtube.com/watch?v=_uQrJ0TkZlc'
-            target='__blank'
-          >
-            <img src={pythonImg} alt='python-img-tag' />
+            href="https://www.youtube.com/watch?v=_uQrJ0TkZlc"
+            target="__blank">
+            <img src={pythonImg} alt="python-img-tag" />
             <p className={styles.skilltxt}>Python</p>
           </a>
           <a
             className={styles.imgcolumn}
-            href='https://www.geeksforgeeks.org/c-plus-plus/ '
-            target='__blank'
-          >
-            <img src={cplusImg} alt='c++-img-tag' />
+            href="https://www.geeksforgeeks.org/c-plus-plus/ "
+            target="__blank">
+            <img src={cplusImg} alt="c++-img-tag" />
             <p className={styles.skilltxt}>C++</p>
           </a>
           <a
             className={styles.imgcolumn}
-            href='https://www.geeksforgeeks.org/c-programming-language/'
-            target='__blank'
-          >
-            <img src={cImg} alt='c-img-tag' />
+            href="https://www.geeksforgeeks.org/c-programming-language/"
+            target="__blank">
+            <img src={cImg} alt="c-img-tag" />
             <p className={styles.skilltxt}>C</p>
           </a>
           <a
             className={styles.imgcolumn}
-            href='https://www.mongodb.com/'
-            target='__blank'
-          >
-            <img src={mongodb} alt='mongodb-img-tag' />
+            href="https://www.mongodb.com/"
+            target="__blank">
+            <img src={mongodb} alt="mongodb-img-tag" />
             <p className={styles.skilltxt}>MongoDB</p>
           </a>
         </div>
