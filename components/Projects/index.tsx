be0dcc83e901d92db799a58c5b0f24d229d2a5eb
Nipun <nipunpatel7@gmail.com>
2024-04-04 12:29:36 +0530

    removed package-lock.json and scroll changes

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -1,62 +1,48 @@
 "use client";
-import { useEffect } from "react";
+
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { constants } from "@/utilities";
 import { Project } from "@/interfaces";
 import Card from "../Card";
 
 const Projects = () => {
-  useEffect(() => {
-    const stickySection = document?.querySelector(".sticky-div");
-    const projects = document.querySelector(".scroll-content");
-    const parentDiv = document.querySelector(".sticky-parent");
+  gsap.registerPlugin(ScrollTrigger);
+  useGSAP(() => {
+    const projects = document.querySelector(".scroll-tag");
 
-    if (parentDiv) {
-      (parentDiv as HTMLElement).style.height = `${
-        (projects as HTMLElement)?.scrollWidth -
-        window.innerWidth +
-        (window.innerWidth <= 500 ? 850 : 930)
-      }px`;
+    const extraWidth = window.innerWidth >= 1024 ? 165 : 110;
+
+    function getScrollAmount() {
+      let projectwidth = projects?.scrollWidth;
+      if (projectwidth) return -(projectwidth - window.innerWidth + extraWidth);
     }
 
-    const handleScroll = () => {
-      if (stickySection) transform(stickySection);
-    };
+    const tween = gsap.to(projects, {
+      x: getScrollAmount as any,
+      duration: 3,
+      ease: "none",
+    });
 
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
-  const transform = (section: Element) => {
-    if (section.parentElement) {
-      const scrollSection = section.querySelector(".scroll-content");
-      const headingElement = document.querySelector(".heading-h3");
-      const headingOffsetTop = (headingElement as HTMLElement).offsetTop;
-      const projects = document.querySelector(".scroll-content");
-      let percentage = window.scrollY - headingOffsetTop;
-      if (
-        scrollSection &&
-        percentage > -20 &&
-        percentage <
-          (projects as HTMLElement)?.scrollWidth -
-            window.innerWidth +
-            (window.innerWidth <= 500 ? 170 : 230)
-      ) {
-        (
-          scrollSection as HTMLElement
-        ).style.transform = `translate3d(${-percentage}px,0,0)`;
-      }
-    }
-  };
+    ScrollTrigger.create({
+      trigger: ".projects-wrapper",
+      start: "top 20%",
+      end: () => `+=${(getScrollAmount?.() as any) * -1}`,
+      pin: true,
+      animation: tween,
+      scrub: 0.4,
+      invalidateOnRefresh: true,
+    });
+  });
 
   return (
-    <section className="bg-gradient-to-b from-[#f377354d] to-[#8383FF] xs:p-4 md:p-6 lg:p-12 sticky-parent">
-      <h3 className="xs:text-xl md:text-3xl lg:text-4xl font-bold mb-2 text-[#333333] heading-h3">
+    <section className="bg-gradient-to-b from-[#f377354d] to-[#8383FF] relative  xs:p-4 md:p-6 lg:p-12">
+      <h3 className="xs:text-xl md:text-3xl lg:text-4xl font-bold mb-2 text-[#333333]">
         Projects
       </h3>
-      <div className="p-12 sticky-div h-[0.9%] min-h-[17rem] my-6">
-        <div className="flex scroll-content gap-3">
+      <div className="p-12 projects-wrapper overflow-y-hidden overflow-x-hidden">
+        <div className="flex gap-3 w-full flex-nowrap my-6 scroll-tag">
           {constants.PORTFOLIO.map((project: Project) => (
             <Card data={project} key={project.name} />
           ))}
